f82db16c1905341047d508f709107e3c038a794d
Daniel Kopecký <kopecky.d@gmail.com>
2026-05-27 21:01:39 +0200

    2026_05_27

---
+++ Lekce 05/infinit_loop.py
@@ -0,0 +1,7 @@
+index = 1
+
+while index < 20:
+    print(index)
+    # neinkrementuji hodnotu v proměnné 'index'
+    # .. hodnota je v každém kroce 1 a smyčka nekončí.
+    # .. Ctrl + C -> přerušení smyčky
